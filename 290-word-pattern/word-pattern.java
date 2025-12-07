06ad19bc21c03483e07e1b5602fc775f2f7ec46f
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-07 13:24:45 -0800

    Time: 1 ms (79.70%) | Memory: 42.5 MB (53.62%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,27 +1,56 @@
+/* Fastest with two maps */
 class Solution {
     public boolean wordPattern(String pattern, String s) {
 
         String[] words = s.split(" ");
-        Map<Character, String> Pattern = new HashMap<>();
-        //System.out.println(pattern.charAt(0));
+        Map<Character, String> c2w = new HashMap<>();
+        Map<String, Character> w2c = new HashMap<>();
+
         if (words.length != pattern.length()) {
             return false;
         }
 
         for (int i = 0; i < words.length; i++) {
             char c = pattern.charAt(i);
-            if (Pattern.containsKey(c)) {
-                if (!Pattern.get(c).equals(words[i])) {
-                    return false;
-                }
-            } else {
-                if (Pattern.containsValue(words[i])) {
+            String w = words[i];
+
+            if (c2w.containsKey(c)) {
+                if (!c2w.get(c).equals(w)) {
                     return false;
                 }
             }
-            Pattern.put(c, words[i]);
+            if (w2c.containsKey(w))
+                if (w2c.get(w) != c) {
+                    return false;
+                }
+            c2w.put(c, w);
+            w2c.put(w, c);
         }
-
         return true;
     }
 }
+/* My first solution */
+
+// class Solution {
+//     public boolean wordPattern(String pattern, String s) {
+//         String[] words = s.split(" ");
+//         Map<Character, String> Pattern = new HashMap<>();
+//         if (words.length != pattern.length()) {
+//             return false;
+//         }
+//         for (int i = 0; i < words.length; i++) {
+//             char c = pattern.charAt(i);
+//             if (Pattern.containsKey(c)) {
+//                 if (!Pattern.get(c).equals(words[i])) {
+//                     return false;
+//                 }
+//             } else {
+//                 if (Pattern.containsValue(words[i])) {
+//                     return false;
+//                 }
+//             }
+//             Pattern.put(c, words[i]);
+//         }
+//         return true;
+//     }
+// }
