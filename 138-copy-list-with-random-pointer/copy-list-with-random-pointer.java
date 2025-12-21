cd348c18ea2f10dbf2ed01d755869ceba55211e8
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2025-12-20 22:11:19 -0800

    Time: 0 ms (100.00%) | Memory: 46.6 MB (34.45%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -13,36 +13,72 @@ class Node {
 }
 */
 
+/** First solutino using Map to set Random in two runs*/
+
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         if (head == null)
+//             return null;
+
+//         Map<Node, Node> track = new HashMap<>();
+//         Node dummy = head;
+//         Node copy = new Node(dummy.val);
+//         Node copyDummy = copy;
+
+//         track.put(dummy, copy);
+//         dummy = dummy.next;
+//         while (dummy != null) {
+//             Node newCopy = new Node(dummy.val);
+//             copyDummy.next = newCopy;
+//             copyDummy = newCopy;
+//             track.put(dummy, copyDummy);
+//             dummy = dummy.next;
+//         }
+//         dummy = head;
+//         copyDummy = copy;
+
+//         while (dummy != null) {
+//             if (dummy.random != null) {
+//                 copyDummy.random = track.get(dummy.random);
+//             }
+//             dummy = dummy.next;
+//             copyDummy = copyDummy.next;
+//         }
+
+//         return copy;
+//     }
+// }
+
 class Solution {
     public Node copyRandomList(Node head) {
         if (head == null)
             return null;
-
-        Map<Node, Node> track = new HashMap<>();
         Node dummy = head;
-        Node copy = new Node(dummy.val);
-        Node copyDummy = copy;
 
-        track.put(dummy, copy);
-        dummy = dummy.next;
         while (dummy != null) {
-            Node newCopy = new Node(dummy.val);
-            copyDummy.next = newCopy;
-            copyDummy = newCopy;
-            track.put(dummy, copyDummy);
-            dummy = dummy.next;
+            Node newDummy = new Node(dummy.val);
+            newDummy.next = dummy.next;
+            dummy.next = newDummy;
+            dummy = newDummy.next;
         }
         dummy = head;
-        copyDummy = copy;
-
         while (dummy != null) {
             if (dummy.random != null) {
-                copyDummy.random = track.get(dummy.random);
+                dummy.next.random = dummy.random.next;
+            }
+            dummy = dummy.next.next;
+        }
+        dummy = head;
+        Node copyHead = head.next;
+
+        while (dummy != null) {
+            Node copy = dummy.next;
+            dummy.next = copy.next;
+            if (copy.next != null) {
+                copy.next = copy.next.next;
             }
             dummy = dummy.next;
-            copyDummy = copyDummy.next;
         }
-
-        return copy;
+        return copyHead;
     }
 }
