2ece6c24557ae4b566da15a6f43c5e62971c5b70
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-06 16:26:52 -0800

    Time: 0 ms (100.00%) | Memory: 49.6 MB (28.48%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    // public int countNodes(TreeNode root) {
-    //     if (root == null) {
-    //         return 0;
-    //     }
-    //     return 1 + countNodes(root.left) + countNodes(root.right);
-    // }
-    int count = 0;
     public int countNodes(TreeNode root) {
-
         if (root == null) {
             return 0;
         }
-        count++;
-        countNodes(root.left);
-        countNodes(root.right);
-
-        return count;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
+    // int count = 0;
+    // public int countNodes(TreeNode root) {
+
+    //     if (root == null) {
+    //         return 0;
+    //     }
+    //     count++;
+    //     countNodes(root.left);
+    //     countNodes(root.right);
+
+    //     return count;
+    // }
 }
