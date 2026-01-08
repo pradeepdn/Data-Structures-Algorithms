4250d52a84196ddad1e94e7d67dbe3d71fd3c698
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-08 00:02:34 -0800

    Time: 0 ms (100.00%) | Memory: 44 MB (24.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        while(i+1 < matrix.length && matrix[i+1][0] <= target){
-            i++; 
-        }
-        // for(int i =0; i<matrix.length; i++){
-             for(int j= 0; j<matrix[0].length; j++){
-                 if(matrix[i][j] == target) return true; 
+    /**Version 1 linear search */
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target)
+                    return true;
             }
-        // }
+        }
         return false;
     }
+
+    /**Version 2 looking for the right colum */
+    //     int i = 0;
+    //     while (i + 1 < matrix.length && matrix[i + 1][0] <= target) {
+    //         i++;
+    //     }
+    //     for (int j = 0; j < matrix[0].length; j++) {
+    //         if (matrix[i][j] == target)
+    //             return true;
+    //     }
+    //     return false;
+    // }
 }
