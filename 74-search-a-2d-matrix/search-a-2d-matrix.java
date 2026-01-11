01bfe38b409dcbb1ee05e9ebb87988d1f7c6987a
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-11 15:04:18 -0800

    Time: 0 ms (100.00%) | Memory: 44.2 MB (12.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,15 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    /**Version 1 linear search */
+        /**Optimal Binary Search */
+        if (matrix == null || matrix.length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = (m * n) - 1;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == target)
-                    return true;
-            }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n];
+
+            if (midVal == target)
+                return true;
+            else if (midVal < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
+
         return false;
     }
+
+    /**Version 1 linear search */
+
+    //     for (int i = 0; i < matrix.length; i++) {
+    //         for (int j = 0; j < matrix[0].length; j++) {
+    //             if (matrix[i][j] == target)
+    //                 return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     /**Version 2 looking for the right colum */
     //     int i = 0;
