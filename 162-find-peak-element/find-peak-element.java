31e937d52ab70748aaf436a6250daa25cef57826
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-12 16:55:56 -0800

    Time: 0 ms (100.00%) | Memory: 44.2 MB (59.05%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,19 +3,38 @@ class Solution {
         if (nums.length <= 1) {
             return 0;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] > nums[i + 1]) {
-                    return i;
-                }
-            } else if (i == nums.length - 1) {
-                if (nums[i] > nums[i - 1]) {
-                    return i;
-                }
-            } else if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return 0;
+        return left;
+
+        /** Linear O(n) */
+        //     if (nums.length <= 1) {
+        //         return 0;
+        //     }
+        //     for (int i = 0; i < nums.length; i++) {
+        //         if (i == 0) {
+        //             if (nums[i] > nums[i + 1]) {
+        //                 return i;
+        //             }
+        //         } else if (i == nums.length - 1) {
+        //             if (nums[i] > nums[i - 1]) {
+        //                 return i;
+        //             }
+        //         } else if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+        //             return i;
+        //         }
+        //     }
+        //     return 0;
+        // }
     }
 }
