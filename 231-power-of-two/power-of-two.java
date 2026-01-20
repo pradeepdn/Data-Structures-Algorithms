980b78de9105fa70b8458bf5ccb6dc82246f6fa6
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-19 16:04:43 -0800

    Time: 1 ms (95.65%) | Memory: 42.5 MB (62.99%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,9 +3,12 @@ class Solution {
         if (n <= 0) {
             return false;
         }
-        while (n % 2 == 0) {
-            n /=2;
-        }
-        return n==1?true:false;
+        // Calculate log base 2: log10(n) / log10(2)
+        double res = Math.log10(n) / Math.log10(2);
+        return res % 1 == 0;
+        // while (n % 2 == 0) {
+        //     n /=2;
+        // }
+        // return n==1?true:false;
     }
 }
