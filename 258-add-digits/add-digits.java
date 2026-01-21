3e65d6542380d03757195ae5cd5a22d30276e5e5
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-21 00:22:17 -0800

    Time: 1 ms (97.67%) | Memory: 42.6 MB (71.14%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
         /**Optimal*/
         // if (num == 0) return 0;
         // if (num % 9 == 0) return 9;
@@ -7,14 +9,14 @@ class Solution {
         /** shortest */
         // return 1 + (num - 1) % 9;
         /** own */
-        while (num > 9) {
-            int sum = 0;
-            while (num != 0) {
-                sum += num % 10;
-                num = num / 10;
-            }
-            num = sum;
-        }
-        return num;
+        // while (num > 9) {
+        //     int sum = 0;
+        //     while (num != 0) {
+        //         sum += num % 10;
+        //         num = num / 10;
+        //     }
+        //     num = sum;
+        // }
+        // return num;
     }
 }
