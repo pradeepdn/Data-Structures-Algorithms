1e580d42c8bdb52b3195c75f7801a9124f0978bb
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-21 17:06:24 -0800

    Time: 2 ms (100.00%) | Memory: 52.9 MB (43.93%) - LeetSync

--- 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
+++ 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.java
@@ -1,16 +1,30 @@
 class Solution {
     public int numTimesAllBlue(int[] flips) {
-        int count = 0;
-        long sum = 0; 
-        for (int i = 0; i < flips.length; i++) {
-            sum += flips[i];
-            
-            long k = i + 1;
-            long n = (k * (k + 1)) / 2;
 
-            if (n == sum)
+        /** Max Value approach */
+        
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < flips.length; i++) {
+            
+            max = Math.max(max, flips[i]);
+            if (i+1 >= max) {
                 count++;
+            }
+            
         }
+        /**Gaussian Sum*/
+        // int count = 0;
+        // long sum = 0;
+        // for (int i = 0; i < flips.length; i++) {
+        //     sum += flips[i];
+
+        //     long k = i + 1;
+        //     long n = (k * (k + 1)) / 2;
+
+        //     if (n == sum)
+        //         count++;
+        // }
         return count;
     }
 }
