ebf79ca9a2d6660ccc5d4ac102a8a643d7528152
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-22 16:04:30 -0800

    Time: 0 ms (100.00%) | Memory: 41.9 MB (77.61%) - LeetSync

---
+++ 292-nim-game/nim-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if (n < 4)
+            return true;
+        if (n % 4 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
