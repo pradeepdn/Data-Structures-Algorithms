62f30d9a714c53b03518c356727fd8afbfe14321
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-28 17:39:54 -0800

    Time: 2 ms (85.56%) | Memory: 45.5 MB (60.45%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        // Create an array based on the bottom row size
+        // Use a primitive int array to avoid Integer object overhead
         int[] dp = new int[n + 1];
 
-        // Start from the bottom row and move up
         for (int i = n - 1; i >= 0; i--) {
+            // Get the current row once to reduce .get() calls
+            List<Integer> currentRow = triangle.get(i);
             for (int j = 0; j <= i; j++) {
-                // dp[j] is updated using the values from the "row below" 
-                // which are currently stored in dp[j] and dp[j+1]
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
-                System.out.println(dp[j]);
+                // Primitive math is significantly faster than object method calls
+                dp[j] = currentRow.get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
         return dp[0];
