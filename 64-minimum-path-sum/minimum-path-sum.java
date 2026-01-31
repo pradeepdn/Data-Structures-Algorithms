bc6637eead8ce85a7c18890d45d320ebb7b8fcd4
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-01-30 21:49:12 -0800

    Time: 4 ms (62.15%) | Memory: 50.8 MB (8.17%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,17 +1,24 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
         
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(i == 0 && j>0){
-                    grid[i][j] = grid[i][j-1] + grid[i][j];
-                }else if(j == 0 && i > 0){
-                    grid[i][j] = grid[i-1][j] +  grid[i][j];
-                }else if(i > 0 && j > 0){
-                    grid[i][j] = Math.min(grid[i-1][j] + grid[i][j], grid[i][j-1]+grid[i][j]);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(i == 0 && j == 0) continue; // Skip the start
+                
+                if(i == 0) {
+                    // First row: can only come from left
+                    grid[i][j] += grid[i][j-1];
+                } else if(j == 0) {
+                    // First column: can only come from top
+                    grid[i][j] += grid[i-1][j];
+                } else {
+                    // Middle of grid: pick the cheaper path
+                    grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
                 }
             }
         }
-        return grid[grid.length-1][grid[0].length-1];
+        return grid[rows - 1][cols - 1];
     }
 }
