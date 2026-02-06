8706911142f5b7c2d2d0e553f02b99e3341fb4f0
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-02-05 17:08:53 -0800

    Time: 0 ms (100.00%) | Memory: 42 MB (84.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,6 +3,7 @@ class Solution {
         if (n <= 2) {
             return n;
         }
+        // return climbStairs(n-1) + climbStairs(n-2); / not optimal Time Limit Exceeds 
         int first = 1;
         int second = 2;
         for (int i = 3; i <= n; i++) {
