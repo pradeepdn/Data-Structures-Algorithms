34ca6904436807c95ae77e58085331af1d2b9a17
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-02-16 14:37:32 -0800

    Time: 0 ms (100.00%) | Memory: 42.7 MB (50.48%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countSegments(String s) {
-
-        if (s.length() == 0 || s == null) {
+        if ( s == null || s.length() == 0) {
             return 0;
         }
         int count = 0;
@@ -21,3 +20,17 @@ class Solution {
         return count;
     }
 }
+
+// class Solution {
+//     public int countSegments(String s) {
+//         int count = 0;
+//         for (int i = 0; i < s.length(); i++) {
+//             // A segment starts if the current char is not a space 
+//             // AND (it's the first char OR the previous char was a space)
+//             if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
