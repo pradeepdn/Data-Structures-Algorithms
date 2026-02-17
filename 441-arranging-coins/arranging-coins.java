08b727202a9be916a47f6d05507c8ff2a4928982
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-02-16 21:56:34 -0800

    Time: 1 ms (99.95%) | Memory: 42.9 MB (24.05%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,6 +1,24 @@
+// class Solution {
+//     public int arrangeCoins(int n) {
+//         double x = (-1 + Math.sqrt(1 + 8.0 * n)) / 2;
+//         return (int) x;
+//     }
+// }
 class Solution {
     public int arrangeCoins(int n) {
-        double x = (-1 + Math.sqrt(1 + 8.0 * n)) / 2;
-        return (int) x;
+        long left = 0, right = n;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long curr = mid * (mid + 1) / 2;
+
+            if (curr == n) return (int) mid;
+
+            if (n < curr) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return (int) right;
     }
 }
