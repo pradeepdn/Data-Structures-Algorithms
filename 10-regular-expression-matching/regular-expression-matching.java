a3ca9a178146ce3de2da476b9ee2a651faa5641b
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-02-17 16:59:22 -0800

    Time: 2 ms (52.91%) | Memory: 43.6 MB (51.74%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true; // Empty string matches empty pattern
+
+        // Handle patterns like a*, a*b*, or a*b*c* matching an empty string
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    // Case 1: Match 0 of the preceding element
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // Case 2: Match 1 or more (if preceding element matches)
+                    char prev = p.charAt(j - 2);
+                    if (prev == s.charAt(i - 1) || prev == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
