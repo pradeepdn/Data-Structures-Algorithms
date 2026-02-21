5c73b4c6cc93f5a85b3eacb66e9c5ac8d9d93c34
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-02-20 16:49:24 -0800

    Time: 2 ms (98.83%) | Memory: 42.6 MB (27.37%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,31 +1,51 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
-
-        HashSet<Integer> prime = new HashSet<Integer>();
-        prime.add(2);
-        prime.add(3);
-        prime.add(5);
-        prime.add(7);
-        prime.add(11);
-        prime.add(13);
-        prime.add(17);
-        prime.add(19);
-
+        // This mask has 1s at positions 2, 3, 5, 7, 11, 13, 17, 19
+        // Binary: 010100010100010101100 (Decimal: 665772)
+        int primeMask = 665772;
         int count = 0;
-        while (left <= right) {
-            int i = left;
-            int bits = 0;
-            while (i > 0) {
-                if (i % 2 == 1) {
-                    bits++;
-                }
-                i = i / 2;
-            }
-            if (prime.contains(bits)) {
+
+        for (int i = left; i <= right; i++) {
+            // Integer.bitCount is a highly optimized hardware-level function
+            int bits = Integer.bitCount(i);
+            
+            // Check if the bit at position 'bits' is a 1 in our mask
+            if (((primeMask >> bits) & 1) == 1) {
                 count++;
             }
-            left++;
         }
+
         return count;
     }
 }
+// class Solution {
+//     public int countPrimeSetBits(int left, int right) {
+
+//         HashSet<Integer> prime = new HashSet<Integer>();
+//         prime.add(2);
+//         prime.add(3);
+//         prime.add(5);
+//         prime.add(7);
+//         prime.add(11);
+//         prime.add(13);
+//         prime.add(17);
+//         prime.add(19);
+
+//         int count = 0;
+//         while (left <= right) {
+//             int i = left;
+//             int bits = 0;
+//             while (i > 0) {
+//                 if (i % 2 == 1) {
+//                     bits++;
+//                 }
+//                 i = i / 2;
+//             }
+//             if (prime.contains(bits)) {
+//                 count++;
+//             }
+//             left++;
+//         }
+//         return count;
+//     }
+// }
