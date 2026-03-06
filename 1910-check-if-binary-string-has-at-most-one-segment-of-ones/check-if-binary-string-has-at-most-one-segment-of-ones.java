96831eee5498d64100c51c08f19b5b0e9b28a42f
Pradeep D N <160975326+pradeepdn@users.noreply.github.com>
2026-03-05 21:22:49 -0800

    Time: 0 ms (100.00%) | Memory: 43 MB (50.94%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,16 +1,27 @@
+// class Solution {
+//     public boolean checkOnesSegment(String s) {
+//         int i = 0;
+//         while (i < s.length()) {
+//             while (i < s.length() && s.charAt(i) - '0' == 0) {
+//                 i++;
+//                 if (i < s.length() && s.charAt(i) - '0' == 1) {
+//                     return false;
+//                 }
+//             }
+//             i++;
+//         }
+//         return true;
+
+//     }
+// }
 class Solution {
     public boolean checkOnesSegment(String s) {
-        int i = 0;
-        while (i < s.length()) {
-            while (i < s.length() && s.charAt(i) - '0' == 0) {
-                i++;
-                if (i < s.length() && s.charAt(i) - '0' == 1) {
-                    return false;
-                }
+        for (int i = 1; i < s.length(); i++) {
+            // We found a '1' that follows a '0'
+            if (s.charAt(i) == '1' && s.charAt(i - 1) == '0') {
+                return false;
             }
-            i++;
         }
         return true;
-
     }
 }
